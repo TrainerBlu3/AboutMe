e75bdcb058de79ec85abee82e7475e88cdff3468
Robert James Walters <mrrobertwalters27@gmail.com>
2025-11-11 16:03:55 -0800

    Project page initial commit
    First work on project page

---
+++ project.js
@@ -0,0 +1,26 @@
+$(document).ready(() =>{
+    $('.project1').mouseenter(() => {
+        $('#proj-img1').fadeIn(200);
+    });
+
+    $('.project1').mouseleave(() => {
+        $('#proj-img1').fadeOut(200);
+    });
+
+
+    $('.project2').mouseenter(() => {
+        $('#proj-img2').fadeIn(200);
+    });
+
+    $('.project2').mouseleave(() => {
+        $('#proj-img2').fadeOut(200);
+    });
+
+    $('.project3').mouseenter(() => {
+        $('#proj-img3').fadeIn(200);
+    });
+
+    $('.project3').mouseleave(() => {
+        $('#proj-img3').fadeOut(200);
+    });
+});
