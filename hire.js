94e6ae58426bc9c7cb45f3592993e374fc7b26f7
Robert James Walters <mrrobertwalters27@gmail.com>
2025-11-20 22:34:58 -0800

    Hire Button Functionality
    Added functionality to hire button. Could maybe make it have a cool animation also of the text or something

---
+++ hire.js
@@ -0,0 +1,15 @@
+function hireTransistion(url) {
+    // Create overlay
+    const overlay = $('<div class="transition-overlay">Good Choice</div>');
+    $('body').append(overlay);
+    
+    // Show it
+    overlay.fadeIn(200);
+    
+    // Navigate after 1.5 seconds
+    setTimeout(() => {
+        window.location.href = url;
+    }, 1500);
+}
+
+
