83289c694bf1a6ecb45ba1109296d5956ba3a6d5
Robert James Walters <mrrobertwalters27@gmail.com>
2025-12-03 10:44:50 -0800

    More contact js updates
    more contact js updates

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-document.getElementById('contactForm').addEventListener('submit', function () {
+document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault(); // Prevent form submission
 
-
-  //   // Clear previous error messages
-  //   document.getElementById(“passwordError”).innerHTML = “”;
-  // // Get input values
   const email = document.getElementById('email').value.trim();
   let isValid = true;
-  // // Validate password
-  if (isValidEmail(email)) {
 
-  }else{
-    //error msg
-    console.log("email error");
+  if(!isValidName(name)){
+
+  }
+ 
+  if (!isValidEmail(email)) {
+    const emailError = document.getElementById('emailError');
+    emailError.textContent = "Please enter a valid email";
+    emailError.classList.add("show");
     isValid = false;
   }
-  //   document.getElementById(“passwordError”).innerHTML = “Password is required.”;
-  //   isValid = false;
-  // } else if (password.length < 8) {
-  //   document.getElementById(“passwordError”).innerHTML = “Password must be at least 8 characters.”;
-  //   isValid = false;
-  // }
+
   // // If all validations pass,
   if (isValid) 
     console.log("Form submitted successfully!");
   //   // Optionally submit the form data to some backend PHP}
 });
 
+function isValidName(name){
+  
+}
+
 function isValidEmail(email) {
   return email.match(/^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,63}$/) !== null;
 }
+
